4fab3f96c1a307783f2742dceaa07c6ae5e2306c
tct123 <42028373+tct123@users.noreply.github.com>
2023-10-02 18:42:28 +0200

    added scripts and changed versionnumber

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
+cd dist/
+rm *
+echo "DELETED"
+cd ..
+python3 -m briefcase create
 python3 -m briefcase package -u
+python3 -m briefcase create --target ubuntu:jammy
 python3 -m briefcase package -u --target ubuntu:jammy
