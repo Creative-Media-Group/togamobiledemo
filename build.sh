6e16b785eb5dbe840877f1639c8a42dcbaa45d15
tct123 <42028373+tct123@users.noreply.github.com>
2023-10-06 03:32:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cd dist/ && rm * && echo "DELETED" && cd .. && python3 -m briefcase create && python3 -m briefcase package -u && python3 -m briefcase create --target ubuntu:jammy && python3 -m briefcase package -u --target ubuntu:jammy && python3 -m briefcase create android
+cd dist && rm * && echo "DELETED" && cd .. && python3 -m briefcase create && python3 -m briefcase package -u && python3 -m briefcase create --target ubuntu:jammy && python3 -m briefcase package -u --target ubuntu:jammy && python3 -m briefcase create android
