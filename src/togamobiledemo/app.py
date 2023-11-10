dd6ce6ba092c2d3f40cabac7f68fd56fd381203c
tct123 <42028373+tct123@users.noreply.github.com>
2023-11-10 23:15:06 +0100

    .

--- src/togamobiledemo/app.py
+++ src/togamobiledemo/app.py
@@ -17,28 +17,29 @@ class TogaMobileDemo(toga.App):
         """
         self.main_box = toga.Box()
         self.x = 0
-        # btn1 = toga.Button(text="Clicker", on_press=self.click)
+        btn1 = toga.Button(text="Clicker", on_press=self.click)
         btnnew = toga.Button(text="New Window", on_press=self.newwindow)
-        # self.text = toga.Label(text=self.text)
-        # self.main_box.add(self.text)
-        # self.main_box.add(btn1)
+        self.maintext = toga.Label(text=self.text)
+        self.main_box.add(self.maintext)
+        self.main_box.add(btn1)
         self.main_box.add(btnnew)
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = self.main_box
         self.main_window.show()
 
     def newwindow(self, widget):
-        apptitle = toga.App().formal_name
-        appauthor = toga.App().author
-        appdescription = toga.App().description
-        appwebsite = toga.App().home_page
-
-        self.main_window.info_dialog(
-            title=apptitle,
-            message=f"App: {apptitle}\nAuthor: {appauthor}\nDescribtion: {appdescription}\nWebsite: {appwebsite}",
-        )
+       apptitle = toga.App().formal_name
+       appauthor = toga.App().author
+       appdescription = toga.App().description
+       appwebsite = toga.App().home_page
+       self.main_window.info_dialog(
+           title=apptitle,
+           message=f"App: {apptitle}\nAuthor: {appauthor}\nDescribtion: {appdescription}\nWebsite: {appwebsite}",
+       )
 
     def click(self, widget):
+        self.text = ""
+        del self.text
         self.x += 1
         self.text = f"You pressed this button {self.x} times."
 
