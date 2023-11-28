e1614929c9da9040da14dab74188cf3eedf189ef
tct123 <42028373+tct123@users.noreply.github.com>
2023-11-28 20:51:38 +0100

    Update app.py

--- src/togamobiledemo/app.py
+++ src/togamobiledemo/app.py
@@ -4,6 +4,7 @@ A Toga Mobile Demo
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+from toga.paths import Paths
 
 
 class TogaMobileDemo(toga.App):
@@ -16,25 +17,34 @@ class TogaMobileDemo(toga.App):
         show the main window.
         """
         self.main_box = toga.Box()
+        # Variables
+        self.path = Paths().app
         self.x = 0
+        # Widgets
         btn1 = toga.Button(text="Clicker", on_press=self.click)
         btnnew = toga.Button(text="New Window", on_press=self.newwindow)
-        
+        img = toga.ImageView(
+            image=toga.Image(path=f"{self.path}/resources/togamobiledemo.png")
+        )
+
+        # add widgets
         self.main_box.add(btn1)
         self.main_box.add(btnnew)
+        self.main_box.add(img)
+        # config
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = self.main_box
         self.main_window.show()
 
     def newwindow(self, widget):
-       apptitle = toga.App().formal_name
-       appauthor = toga.App().author
-       appdescription = toga.App().description
-       appwebsite = toga.App().home_page
-       self.main_window.info_dialog(
-           title=apptitle,
-           message=f"App: {apptitle}\nAuthor: {appauthor}\nDescribtion: {appdescription}\nWebsite: {appwebsite}",
-       )
+        apptitle = toga.App().formal_name
+        appauthor = toga.App().author
+        appdescription = toga.App().description
+        appwebsite = toga.App().home_page
+        self.main_window.info_dialog(
+            title=apptitle,
+            message=f"App: {apptitle}\nAuthor: {appauthor}\nDescribtion: {appdescription}\nWebsite: {appwebsite}",
+        )
 
     def click(self, widget):
         self.text = ""
@@ -46,7 +56,6 @@ class TogaMobileDemo(toga.App):
 
     def abt(widget):
         toga.App.about()
-        pass
 
 
 def main():
